29efbdb5109b316f06a354892714cd1f42c24045
Genady <genady.sergeev@progress.com>
2018-02-23 10:40:44 +0200

    chore: added basic implementation of docs server

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp-help')(require('gulp'));
+const tasks = require('@telerik/kendo-common-tasks');
+const docsServer = require('@telerik/kendo-common-tasks/docs-server');
+
+
+gulp.task('docs', (done) => docsServer('kendo-ui-builder', (browserSync) => {
+        gulp.watch('docs/**/*.md').on('change', browserSync.reload);
+    }, () => {})
+);
+
